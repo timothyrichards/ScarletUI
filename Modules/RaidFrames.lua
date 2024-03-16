1dccc249c047dc0464422c59a439e394c4b63380
Timothy Richards <timothysrichards97@gmail.com>
2024-03-16 16:53:17 -0700

    - fix raid frame profiles for real this time

--- Modules/RaidFrames.lua
+++ Modules/RaidFrames.lua
@@ -1,56 +1,63 @@
 local AceConfigRegistry = LibStub("AceConfigRegistry-3.0")
 
 function ScarletUI:SetupRaidProfiles()
-    local cufLoaded = IsAddOnLoaded("Blizzard_CompactRaidFrames");
     local raidFramesModule = self.db.global.raidFramesModule
-    if not cufLoaded or not raidFramesModule.enabled or self.lightWeightMode or self.retail or self.inCombat then
+    if not raidFramesModule.enabled or self.lightWeightMode or self.retail or self.inCombat then
         return
     end
 
     self.movingRaidFrames = false
+    self.updatingSettings = false
     if not self.raidProfileEventRegistered then
-        self.raidProfileEventRegistered = true;
-        local frame = CreateFrame("Frame", "SUI_RaidFrame", SUI_Frame)
-        frame:RegisterEvent("COMPACT_UNIT_FRAME_PROFILES_LOADED")
-        frame:SetScript("OnEvent", function (_, event, ...)
-            if event == "COMPACT_UNIT_FRAME_PROFILES_LOADED" then
-                ScarletUI:SetupRaidProfiles()
-            end
-        end)
+        local functionExecuted = false
+        local frame = CreateFrame("Frame")
+        frame:SetScript("OnUpdate", function (_, ...)
+            if HasLoadedCUFProfiles() and not functionExecuted then
+                ScarletUI:UpdateProfileOptions()
 
-        hooksecurefunc("SetRaidProfileSavedPosition", function()
-            if not ScarletUI.movingRaidFrames then
-                local profile = GetActiveRaidProfile()
-                local _, _, top, _, bottom, _, left = GetRaidProfileSavedPosition(profile)
-                local options = raidFramesModule.profiles[profile]
-                if options ~= nil and options.move then
-                    options.y = top
-                    options.height = bottom
-                    options.x = left
-                end
+                hooksecurefunc("SetRaidProfileSavedPosition", function()
+                    if not ScarletUI.movingRaidFrames then
+                        local profile = GetActiveRaidProfile()
+                        local _, _, top, _, bottom, _, left = GetRaidProfileSavedPosition(profile)
+                        local options = raidFramesModule.profiles[profile]
+                        if options ~= nil and options.move then
+                            options.y = top
+                            options.height = bottom
+                            options.x = left
+                        end
 
-                AceConfigRegistry:NotifyChange("ScarletUI")
-            end
-        end)
+                        AceConfigRegistry:NotifyChange("ScarletUI")
+                    end
+                end)
 
-        hooksecurefunc("CompactUnitFrameProfiles_ApplyCurrentSettings", function()
-            local profile = GetActiveRaidProfile()
-            if raidFramesModule.profiles[profile] then
-                for k, v in pairs(raidFramesModule.profiles[profile]) do
-                    if not (k == "move" or k == "x" or k == "y" or k == "height") then
-                        local currentValue = tostring(v)
-                        local targetValue = tostring(GetRaidProfileOption(profile, k))
-                        if currentValue ~= targetValue then
-                            raidFramesModule.profiles[profile][k] = GetRaidProfileOption(profile, k)
+                hooksecurefunc("CompactUnitFrameProfiles_ApplyCurrentSettings", function()
+                    if not ScarletUI.updatingSettings then
+                        local profile = GetActiveRaidProfile()
+                        if raidFramesModule.profiles[profile] then
+                            for k, v in pairs(raidFramesModule.profiles[profile]) do
+                                if not (k == "move" or k == "x" or k == "y" or k == "height") then
+                                    local currentValue = tostring(v)
+                                    local targetValue = tostring(GetRaidProfileOption(profile, k))
+                                    if currentValue ~= targetValue then
+                                        raidFramesModule.profiles[profile][k] = GetRaidProfileOption(profile, k)
+                                    end
+                                end
+                            end
                         end
                     end
-                end
+                end)
+
+                functionExecuted = true
+                frame:SetScript("OnUpdate", function() end)
             end
         end)
 
-        return
+        self.raidProfileEventRegistered = true;
     end
+end
 
+function ScarletUI:UpdateProfileOptions()
+    local raidFramesModule = self.db.global.raidFramesModule
     for profile, options in pairs(raidFramesModule.profiles) do
         -- Create a new raid profile if it doesn't exist
         if not RaidProfileExists(profile) then
@@ -59,6 +66,7 @@ function ScarletUI:SetupRaidProfiles()
         end
 
         -- Update settings if the profile does exist
+        self.updatingSettings = true
         if RaidProfileExists(profile) then
             -- Check and apply Raid Profile settings
             for k, v in pairs(options) do
@@ -74,6 +82,7 @@ function ScarletUI:SetupRaidProfiles()
             -- Update position
             ScarletUI:UpdateProfilePositions()
         end
+        self.updatingSettings = false
     end
 end
 
@@ -88,6 +97,7 @@ function ScarletUI:UpdateProfilePositions()
         local _, _, top, _, bottom, _, left = GetRaidProfileSavedPosition(profile)
         if top ~= options.y or bottom ~= options.height or left ~= options.x then
             SetRaidProfileSavedPosition(profile, false, 'TOP', options.y, 'BOTTOM', options.height, 'LEFT', options.x)
+            StaticPopup_Show('SCARLET_UI_RELOAD_DIALOG')
         end
     end
     self.movingRaidFrames = false
