a883dc17cd7a0f5bf259559ab20b21defde3709c
Timothy Richards <timothysrichards97@gmail.com>
2024-05-03 01:56:25 -0700

    - add GuildMicroButton to micro bar

--- Modules/Actionbars.lua
+++ Modules/Actionbars.lua
@@ -83,6 +83,7 @@ local function microBar(actionbarsModule)
         "AchievementMicroButton",
         "QuestLogMicroButton",
         "SocialsMicroButton",
+        "GuildMicroButton",
         "CollectionsMicroButton",
         "PVPMicroButton",
         "LFGMicroButton",
